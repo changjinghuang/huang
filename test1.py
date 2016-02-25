483e7b956a05083d8860617f8db96ee461df6140
changjing.huang <changjing.huang@ef.com>
2016-02-25 10:55:18 +0800

    hello world

--- test1.py
+++ test1.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 
 
+
+
 driver = webdriver.Firefox()
-driver.get('http://www.python.org')
-assert 'python' in driver.title
+driver.implicitly_wait(10)
+driver.get("http://somedomain/url_that_delays_loading")
+myDynamicElement = driver.find_element_by_id("myDynamicElement")
