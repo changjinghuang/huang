ba50169f5bdc9881513c5869a04a9b6a7c039ca1
changjing.huang <changjing.huang@ef.com>
2016-02-25 11:19:47 +0800

    hello world

--- test1.py
+++ test1.py
@@ -7,3 +7,6 @@ driver = webdriver.Firefox()
 driver.implicitly_wait(10)
 driver.get("http://somedomain/url_that_delays_loading")
 myDynamicElement = driver.find_element_by_id("myDynamicElement")
+
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.ID,'someid')))
