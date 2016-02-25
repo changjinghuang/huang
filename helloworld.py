4d82fc7ce800a1e4a95a0c1298d1f03b9086b965
changjing.huang <changjing.huang@ef.com>
2016-02-25 11:49:46 +0800

    hello world

--- helloworld.py
+++ helloworld.py
@@ -5,4 +5,9 @@ driver = webdriver.Firefox()
 driver.get('http://www.python.org')  
 assert 'python' in driver.title  
 elem = driver.find_element_by_name('q')  
-elem.send_keys('pycon')  
+elem.send_keys('pycon')
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get("http://somedomain/url_that_delays_loading")
+myDynamicElement = driver.find_element_by_id("myDynamicElement")
